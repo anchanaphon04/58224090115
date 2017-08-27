bf7494beae13848ef437adbbdf353a85685357e1
anchanaphon04 <31381747+anchanaphon04@users.noreply.github.com>
2017-08-27 16:21:19 +0700

    2-38

---
+++ 38.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf ("%s \n","58224090115");
+	printf ("%s \n","Anchanaphon Munkat");
+	printf ("%s \n","04/11/37");
+	printf ("%119s \n","http://www.npru.ac.th");
+	printf ("%59s \n","bye bye");
+	return 0;
+}
