25c94b73e556b7f3d7360d661972a05f1071da57
anchanaphon04 <31381747+anchanaphon04@users.noreply.github.com>
2017-08-27 14:43:21 +0700

    Initial commit

---
+++ 2-18.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf ("Hello World!\n");
+	printf ("This is my first Program.");
+	printf ("\n\tI\'m a programmer.");
+	return 0;
+}
---
+++ 2-21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf ("My name is : %.2s\n","Kmitl");
+	printf ("My point : %d\n",10+40+49);
+	printf ("Grade : %c\n",'A');
+	printf ("GPA : %.2f\n",3.99);
+	printf ("A = %c\n",65);
+	printf ("%d-%d-%d",17,12,2530); //17-12-2530
+	return 0;
+}
