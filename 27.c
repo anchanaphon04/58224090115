2db8b446a0618a08e61efa3c6f6f517bc3c31b9b
anchanaphon04 <31381747+anchanaphon04@users.noreply.github.com>
2017-08-27 15:24:59 +0700

    add source 27

---
+++ 27.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("123456789012345678901234567890");
+	printf("\n%20d*",46);
+	printf("\n%-20d*",46);
+	printf("\n%3d*",46);
+	printf("\n%3d*",2550);
+	return 0;
+}
