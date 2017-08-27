c78ec9a173e48f00050b0f85b6d484b9b66dbc85
anchanaphon04 <31381747+anchanaphon04@users.noreply.github.com>
2017-08-27 15:53:50 +0700

    Add source 34

---
+++ 34.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf ("\n%-8s: %-13s","Name","Anchanaphon\n");
+	printf ("\n%-8s: %-13s","Surname","Munkat\n");
+	printf ("\n%-8s: %-13d","Age",22);
+	return 0;
+}
