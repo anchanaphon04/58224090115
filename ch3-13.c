8c33ae30f7346763aa4bd543fa4a4eab7daeb150
anchanaphon04 <31381747+anchanaphon04@users.noreply.github.com>
2017-09-03 15:56:34 +0700

    ch3-13

---
+++ ch3-13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int number=25;
+	int a=1,b=2,c=3;
+	float real =99.99;
+	float point1=45.2, point2=30;
+	char choice='a';
+	char ch1='0', ch2='z';
+	
+	printf("number = %d\n",number);
+	printf("a=%d b=%d c=%d\n",1,2,3);
+	printf("real = %.2f\n",real);
+	printf("point1 = %.1f point2 = %.0f\n",point1,point2);
+	printf("choice = %c\n",choice);
+	printf("ch1 = %.0f ch2 = %c\n",ch1,ch2);
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int number=25;
+	int a=1,b=2,c=3;
+	float real =99.99;
+	float pointl=45.2, point2=30;
+	char choice='a';
+	char ch1='0', ch2='z';
+	
+	printf("number = %d\n",number);
+	printf("%d\n",a=1,b=2,c=3);
+	
+}
